3105a66e4781650b6ab793c1768d3e5c4e66ae01
Elliot Kim <elliotkim916@gmail.com>
2018-08-14 14:35:22 -0700

    Added a TOKEN

--- client/src/config.js
+++ client/src/config.js
@@ -4,6 +4,7 @@ export const ARTIST_ENDPOINT = "artists/";
 export const TOP_TRACKS_ENDPOINT = "/top-tracks";
 export const ALBUMS_ENDPOINT = "/albums";
 export const ALBUMS_TRACKS_ENDPOINT = "/tracks";
+export const TOKEN = "BQDIdxKIR-hwjRv_UsUDIDUvQVhWV_RN4WEHgC_fiiVXCCSh1UhDmVZ3fpGeMd8JWQsvbZGbcY-sSxBOM95_6RyE59pqIqWWeYdYzdYiwPkTB99BrObw07aJGFgJLEe7LeLwFHUDMwV8ysqCV_jwWSk4FjY-oKfKMvc";
 
 // get an artist: https://api.spotify.com/v1/artists/{id}
 // get an artists top tracks: https://api.spotify.com/v1/artists/{id}/top-tracks 
