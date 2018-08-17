48dd780e84af8f76b54795f6299b73df5a2af4a1
Elliot Kim <elliotkim916@gmail.com>
2018-08-17 13:29:25 -0700

    Added   - the Genius API base url   - the Genius token

--- client/src/config.js
+++ client/src/config.js
@@ -1,12 +1,10 @@
 export const SPOTIFY_SEARCH_URL = "https://api.spotify.com/v1/search";
 export const SPOTIFY_BASE_URL = "https://api.spotify.com/v1/";
-export const ARTIST_ENDPOINT = "artists/";
-export const TOP_TRACKS_ENDPOINT = "/top-tracks";
-export const ALBUMS_ENDPOINT = "/albums";
-export const ALBUMS_TRACKS_ENDPOINT = "/tracks";
-export const TOKEN = "BQDIdxKIR-hwjRv_UsUDIDUvQVhWV_RN4WEHgC_fiiVXCCSh1UhDmVZ3fpGeMd8JWQsvbZGbcY-sSxBOM95_6RyE59pqIqWWeYdYzdYiwPkTB99BrObw07aJGFgJLEe7LeLwFHUDMwV8ysqCV_jwWSk4FjY-oKfKMvc";
+export const ARTIST_ENDPOINT = "artists/";          // get an artist: https://api.spotify.com/v1/artists/{id}
+export const TOP_TRACKS_ENDPOINT = "/top-tracks";   // get an artists top tracks: https://api.spotify.com/v1/artists/{id}/top-tracks 
+export const ALBUMS_ENDPOINT = "/albums";           // get an artists albums: https://api.spotify.com/v1/artists/{id}/albums
+export const ALBUMS_TRACKS_ENDPOINT = "/tracks";    // get an albums tracks: ttps://api.spotify.com/v1/albums/{id}/tracks
+export const SPOTIFY_TOKEN = "BQBUdVJksw9l1N7Vy45Koo_uKg1n9sLtUV_5WyhrPayISP1jOgouAC78OAT7Qy8VJ4yqfyPS4yQma70EOZTm6oeEO7Fc6BKiLqtXUTk4gAL4M4DgTWHjBJx9lSYKNJ6gDgWzZHBRIW1PjZnVJeCWOLfn-U5RjEPsE_k";
 
-// get an artist: https://api.spotify.com/v1/artists/{id}
-// get an artists top tracks: https://api.spotify.com/v1/artists/{id}/top-tracks 
-// get an artists albums: https://api.spotify.com/v1/artists/{id}/albums
-// get an albums tracks: ttps://api.spotify.com/v1/albums/{id}/tracks
+export const GENIUS_BASE_URL = "https://api.genius.com/search?q=";
+export const GENIUS_TOKEN = "YCxrPJP6GA4-oiSSis1vWpsVdmEiGQ1PuxfIn8yRiE62GfBv1_Xbv0pQ6OZ_VIu6";
