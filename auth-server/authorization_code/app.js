5fc31762cae87541002d634aae92acf579532bf6
Elliot Kim <elliotkim916@gmail.com>
2018-08-28 18:28:31 -0700

    Changed scope

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -47,7 +47,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-read-playback-state';
+  var scope = 'streaming user-read-birthdate user-read-private user-read-email user-read-playback-state';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
