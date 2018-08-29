d7444849e8ede19b084f591da10d3272d40b0212
Elliot Kim <elliotkim916@gmail.com>
2018-08-28 18:23:42 -0700

    Added Genius authentication to make API call

--- client/src/config.js
+++ client/src/config.js
@@ -4,7 +4,10 @@ export const ARTIST_ENDPOINT = "artists/";          // get an artist: https://ap
 export const TOP_TRACKS_ENDPOINT = "/top-tracks";   // get an artists top tracks: https://api.spotify.com/v1/artists/{id}/top-tracks 
 export const ALBUMS_ENDPOINT = "/albums";           // get an artists albums: https://api.spotify.com/v1/artists/{id}/albums
 export const ALBUMS_TRACKS_ENDPOINT = "/tracks";    // get an albums tracks: ttps://api.spotify.com/v1/albums/{id}/tracks
-export const SPOTIFY_TOKEN = "BQBUdVJksw9l1N7Vy45Koo_uKg1n9sLtUV_5WyhrPayISP1jOgouAC78OAT7Qy8VJ4yqfyPS4yQma70EOZTm6oeEO7Fc6BKiLqtXUTk4gAL4M4DgTWHjBJx9lSYKNJ6gDgWzZHBRIW1PjZnVJeCWOLfn-U5RjEPsE_k";
+export const SPOTIFY_TOKEN = "BQAkgm8k5E2OTPmtQy1psrVbROBa5_yFzgxxuuxttqJ_e11dNTrKWsIiRK8Rkt82cl7dxZ-AKzvoPhDAn5FOhKsqyonCFjVnl1BipQT_aS3hgPus4RN7bxORjlON8CkdJ3cMyoafmB-qGGK5mFDuTQu8T9QVEFAL3oqRis15nQ";
 
 export const GENIUS_BASE_URL = "https://api.genius.com/search?q=";
-export const GENIUS_TOKEN = "YCxrPJP6GA4-oiSSis1vWpsVdmEiGQ1PuxfIn8yRiE62GfBv1_Xbv0pQ6OZ_VIu6";
+export const GENIUS_OAUTH_BASE_URL = "https://api.genius.com/oauth/authorize?";
+export const GENIUS_CLIENT_ID = "CmmRGl7ZYAwZjWzuLQqsfnJ06Z0VgT9c2NYiclYDObanQdg43bvM1auv_S28YdEf";
+export const GENIUS_SECRET = "Yk1_wt6f--PvU7WPjqGW80qDzClBWC2kAXGw6RTlhT4njJUp7y5gUJ-UINCKyr_wT0vw3uHB3nXyP1iR5E9ZeA";
+export const GENIUS_TOKEN = "peQhsQGMnyW69YZvpkLMRKqbz1HrkzXKCSbsWfihSUhGdBmGtaC4o5YUKZqR_9h2";
