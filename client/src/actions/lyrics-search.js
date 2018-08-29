15264fd195ca79d0846a2cfa67204aaa6ba746ec
Elliot Kim <elliotkim916@gmail.com>
2018-08-28 18:27:43 -0700

    Added access token to fetch in fetch_lyrics function

--- client/src/actions/lyrics-search.js
+++ client/src/actions/lyrics-search.js
@@ -1,6 +1,9 @@
 import {
   GENIUS_BASE_URL,
+  // GENIUS_OAUTH_BASE_URL,
   GENIUS_TOKEN
+  // GENIUS_CLIENT_ID,
+  // GENIUS_SECRET
 } from '../config';
 
 export const LYRICS_SEARCH_REQUEST = 'LYRICS_SEARCH_REQUEST';
@@ -20,17 +23,21 @@ export const lyricsSearchError = error => ({
   error
 });
 
+// function OAuth() {
+//   return fetch(`${GENIUS_OAUTH_BASE_URL}client_id=${GENIUS_CLIENT_ID}&scope=`)
+// }
+
 function fetch_lyrics(song) {
-  return fetch(`${GENIUS_BASE_URL}${song}`,{
-    headers: {
-      'Authorization': `Bearer ${GENIUS_TOKEN}`
-    }
+  return fetch(`${GENIUS_BASE_URL}${song}&access_token=${GENIUS_TOKEN}`,{
+    // headers: {
+    //   'Authorization': `Bearer ${GENIUS_TOKEN}`
+    // }
   }).then(res => {
     if (!res.ok) {
       return Promise.reject(res.statusText);
     }
     return res.json();
-  }).then(data => console.log(data)); 
+  }).then(data => data.response.hits); 
 }
 
 export const get_lyrics = lyrics => dispatch => {
