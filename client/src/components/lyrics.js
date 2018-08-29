c608316c375400351f5fc90e719401a9cc2b0bd7
Elliot Kim <elliotkim916@gmail.com>
2018-08-28 18:22:19 -0700

    Added lyrics component

---
+++ client/src/components/lyrics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './lyrics.css';
+
+export class Lyrics extends React.Component {
+  render() {
+    return (
+      <section className="lyrics_section">
+        
+      </section>
+    )
+  }
+}
