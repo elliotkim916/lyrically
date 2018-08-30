0e7d3fdc2e7324db1e3b8905e971acd3c4f692c3
Elliot Kim <elliotkim916@gmail.com>
2018-08-29 17:19:33 -0700

    Added GENIUS_SONG_BASE_URL

--- client/src/config.js
+++ client/src/config.js
@@ -7,6 +7,7 @@ export const ALBUMS_TRACKS_ENDPOINT = "/tracks";    // get an albums tracks: ttp
 export const SPOTIFY_TOKEN = "BQAkgm8k5E2OTPmtQy1psrVbROBa5_yFzgxxuuxttqJ_e11dNTrKWsIiRK8Rkt82cl7dxZ-AKzvoPhDAn5FOhKsqyonCFjVnl1BipQT_aS3hgPus4RN7bxORjlON8CkdJ3cMyoafmB-qGGK5mFDuTQu8T9QVEFAL3oqRis15nQ";
 
 export const GENIUS_BASE_URL = "https://api.genius.com/search?q=";
+export const GENIUS_SONG_BASE_URL = "https://api.genius.com";
 export const GENIUS_OAUTH_BASE_URL = "https://api.genius.com/oauth/authorize?";
 export const GENIUS_CLIENT_ID = "CmmRGl7ZYAwZjWzuLQqsfnJ06Z0VgT9c2NYiclYDObanQdg43bvM1auv_S28YdEf";
 export const GENIUS_SECRET = "Yk1_wt6f--PvU7WPjqGW80qDzClBWC2kAXGw6RTlhT4njJUp7y5gUJ-UINCKyr_wT0vw3uHB3nXyP1iR5E9ZeA";
