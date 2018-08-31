31298754ef7a8cc4bd4ff2deb6943e9e57ac0338
Elliot Kim <elliotkim916@gmail.com>
2018-08-31 14:24:19 -0700

    Added token to fetch request in fetch_song function

--- client/src/actions/song-search.js
+++ client/src/actions/song-search.js
@@ -1,12 +1,12 @@
 import {
   GENIUS_SONG_BASE_URL,
-  GENIUS_TOKEN,
-  GENIUS_OAUTH_BASE_URL,
-  GENIUS_CLIENT_ID,
-  GENIUS_REDIRECT_URI
+  GENIUS_TOKEN
+  //GENIUS_OAUTH_BASE_URL,
+  //GENIUS_CLIENT_ID,
+  //GENIUS_OAUTH_AUTHORIZE_BASE_URL,
+  //GENIUS_REDIRECT_URI,
+  //GENIUS_SECRET
 } from '../config';
-
-let href;
 
 export const SONG_SEARCH_REQUEST = 'SONG_SEARCH_REQUEST';
 export const songSearchRequest = () => ({
@@ -25,31 +25,55 @@ export const songSearchError = error => ({
   error
 });
 
-function OAuth() {
-  window.location.href = `${GENIUS_OAUTH_BASE_URL}client_id=${GENIUS_CLIENT_ID}&redirect_uri=http://localhost:3000/&scope=me&response_type=code`;
-  href = window.location.href.replace(/.*code=/g,'');
-  console.log(href);
-}
+// function OAuth() {
+//   window.location.href = `${GENIUS_OAUTH_BASE_URL}client_id=${GENIUS_CLIENT_ID}&redirect_uri=http://localhost:3000/&scope=me&response_type=code`;
+//   let href = window.location.href.replace(/.*code=/g,'');
+//   console.log(href);
+// }
 
-function fetch_song(song_endpoint) {
+// function authorizeOAuth() {
+//   let current_token = localStorage.getItem('genius_token');
+//   console.log(current_token);
+//   const headers = {
+//     'Authorization': `Bearer ${GENIUS_TOKEN}`
+//   }
+
+//   const response_data = {
+//     "code": current_token,
+//     "client_id": `${GENIUS_CLIENT_ID}`,
+//     "client_secret": `${GENIUS_SECRET}`,
+//     "redirect_uri": "http://localhost:3000/",
+//     "response_type": "token",
+//     "grant_type": "authorization_code"
+//   }
+
+//   return fetch(`${GENIUS_OAUTH_AUTHORIZE_BASE_URL}`, {
+//     headers,
+//     response_data
+//     }).then(res => console.log(res))
+// }
+
+function fetch_song(endpoint, token) {
+  console.log(token);
   const headers = {
     'Authorization': `Bearer ${GENIUS_TOKEN}`
   }
   
-  return fetch(`${GENIUS_SONG_BASE_URL}${song_endpoint}`, {
+  return fetch(`${GENIUS_SONG_BASE_URL}${endpoint}&${token}`, {
     headers
   }).then(res => {
     if (!res.ok) {
       return Promise.reject(res.statusText);
     }
     return res.json();
-  }).then(data => console.log(data)); 
+  }).then(data => console.log(data)).then(() => localStorage.removeItem('genius_token')); 
 }
 
-export const get_song = song => dispatch => {
+export const get_song = (endpoint, token) => dispatch => {
   dispatch(songSearchRequest());
-  OAuth();
-  fetch_song(song)
+  //OAuth();
+  //authorizeOAuth();
+  fetch_song(endpoint, token)
   .then(song => {
     dispatch(songSearchSuccess(song))
   }).catch(error => {
